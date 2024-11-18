b3b3ffe3dd568a56ac3c09ced5b5c1ea18775850
Jason Ye <yzm0614@gmail.com>
2024-11-18 03:10:30 -0500

    URP post process settings and save point rock

---
+++ Assets/Scripts/Save Load/Save Point.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePoint : MonoBehaviour, IInteractable
+{
+    public VoidEvent_SO saveGameEvent;
+    public SpriteRenderer spriteRenderer;
+    public Sprite savePointOn;
+    public Sprite savePointOff;
+    public GameObject lightObj;
+    private bool isDone;
+
+    private void OnEnable()
+    {
+        spriteRenderer.sprite = isDone ? savePointOn : savePointOff;
+        lightObj.SetActive(isDone);
+    }
+
+    public void TriggerAction()
+    {
+        if (!isDone)
+        {
+            isDone = true;
+            spriteRenderer.sprite = savePointOn;
+            GetComponent<Collider2D>().enabled = false;
+            lightObj.SetActive(true);
+        }
+    }
+}
